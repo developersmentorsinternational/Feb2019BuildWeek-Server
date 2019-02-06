464b259a8d09b23845e276afc2f5ac7b1ee4fe47
dan2014 <dmen326@gmail.com>
2019-02-06 16:47:24 -0500

    trying to fix the .gitignore file

--- testFunctions.js
+++ testFunctions.js
@@ -9,6 +9,15 @@ db.select(["people.id","creds.email","creds.password","firstName","lastName","co
 .then(res => console.log(res))
 .catch(err => console.log(err))
 
+db.select(["people.id","creds.email","creds.password","firstName","lastName","countryCodes.countryCode","types.type","regions.region"]).from('people')
+.innerJoin('countryCodes', 'people.countryCode', 'countryCodes.id')
+.innerJoin('types', 'people.type', 'types.id')
+.innerJoin('regions', 'people.region', 'regions.id')
+.innerJoin('creds', 'people.id', 'creds.mentor')
+.where({"creds.email":"dan@dan.com"})
+.then(res => console.log(res))
+.catch(err => console.log(err))
+
 // SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
 // FROM Orders
 // INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
